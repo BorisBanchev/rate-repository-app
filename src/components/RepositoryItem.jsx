bd0bf7bbe46acb2b81282cb1356be95c9475cf0c
BorisBanchev <boris.banchev03@gmail.com>
2025-10-05 16:05:59 +0300

    Add exercise 10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const RepositoryItem = ({
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazerCount,
+  ratingAverage,
+  reviewCount,
+}) => {
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description} </Text>
+      <Text>Language: {language} </Text>
+      <Text>Stars: {stargazerCount} </Text>
+      <Text>Forks: {forksCount} </Text>
+      <Text>Reviews: {reviewCount} </Text>
+      <Text>Rating: {ratingAverage} </Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
